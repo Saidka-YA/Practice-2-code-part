a968c857a389a2cfbc0819a2e101abe3aae23173
Saidka-YA <pochoevsaidullo4@gmail.com>
2026-06-02 02:59:05 +0700

    Исправил ошибку сохранения файлов

---
+++ task1/funcktions.h
@@ -0,0 +1,10 @@
+#ifndef FUNCKTIONS_H
+#define FUNCKTIONS_H
+#include <cstdlib>
+
+bool isPrime(uint n);
+uint gcd(uint a, uint b);
+uint binaryModPow(uint base, uint exponent, uint mod);
+uint fermaMethod(uint a, uint x, uint p, bool &flag);
+
+#endif
